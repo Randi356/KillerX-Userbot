9c8ca682d7d9c71d6403f21cf87e73485407ad4c
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-11-13 03:29:56 +0700

    Vegeta-Userbot support

--- termux_install.sh
+++ termux_install.sh
@@ -34,8 +34,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/ramadhani892/RAM-UBOT.git
-cd Lord-Userbot
+git clone https://github.com/Randi356/VEGETA-USERBOT.git
+cd VEGETA-USERBOT
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
