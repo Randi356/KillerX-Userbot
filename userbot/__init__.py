7a3f69cb22a49f354a8b379ca49891feeddada99
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2021-11-26 04:25:16 +0700

    Fix button

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -621,6 +621,13 @@ with bot:
                        ]
                 )
 
+       @tgbot.on(events.CallbackQuery(data=b"close"))
+        async def close(event):
+            buttons [
+                (custom.button.inline("Open Menu", data="open_plugin"),),
+            ]
+            await event.edit("Menu Tutup! ", buttons=Buttons)
+
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"helpme_prev\((.+?)\)")
@@ -671,13 +678,6 @@ with bot:
                 reply_pop_up_alert = f"WOI NGENTOT!! JANGAN PAKE PUNYA {DEFAULTUSER} DONG BABI."
 
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-        @tgbot.on(events.CallbackQuery(data=b"close"))
-        async def close(event):
-            buttons [
-                (custom.button.inline("Open Menu", data="open_plugin"),),
-            ]
-            await event.edit("Menu Tutup!", buttons=Buttons)
 
 
     except BaseException:
