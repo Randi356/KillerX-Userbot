0ee4e4ad636bb092f1aa32f0cac9c445169c6025
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2021-11-30 14:26:48 +0700

    Vegeta-Userbot pull #60

--- Getstring.sh
+++ Getstring.sh
@@ -57,7 +57,7 @@ sleep 2
 
 if [ ! -e string_session.py ]; then
     echo -e "\nDownloading string_session.py\n"
-    wget https://raw.githubusercontent.com/Randi356/VEGETA-USERBOT/VEGETA-USERBOT/string_session.py
+    wget https://raw.githubusercontent.com/Randi356/Vegeta-Userbot/Vegeta-Userbot/string_session.py
 
     echo -e "\nRunning script...\n"
     sleep 1
