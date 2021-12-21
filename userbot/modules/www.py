e6bdc0e493bd7b3faec0e626e5073f816ddaf0aa
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2021-12-21 16:03:04 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -28,7 +28,7 @@ from userbot.events import register
 # OWN BY RENDY
 # LU TAMBAH MODULES KONTOL
 
-absen = [
+mamakk = [
     "**Hadir cakep dinda** 🥵",
     "**Hadir bang Rendy** 😎",
     "**Hadir kak** 😉",
@@ -36,7 +36,7 @@ absen = [
     "**Hadir kak maap telat** 🥺",
 ]
 
-pacar = [
+gayy = [
     "**Kamu mau jadi pacar aku ga?** 💘",
     "**mmk mending sama aku** 😎",
     "**Hai ganteng** 😍",
@@ -74,13 +74,13 @@ async def get_readable_time(seconds: int) -> str:
 # CREDIT RANDI356
 # DONT'T REMOVE CREDIT FUCK DICK
 
-@register(incoming=True, from_users=DEVS, pattern=r"^.absen$")
+@register(incoming=True, from_users=DEVS, pattern=r"^mamakk$")
 async def _(rendy):
-    await rendy.reply(random.choice(absen))
+    await rendy.reply(random.choice(mamakk))
 
-@register(incoming=True, from_users=DEVS, pattern=r"^.pacar$")
+@register(incoming=True, from_users=DEVS, pattern=r"^gayy$")
 async def _(kontol):
-    await kontol.reply(random.choice(pacar))
+    await kontol.reply(random.choice(gayy))
 
 @register(outgoing=True, pattern="^.pings$")
 
