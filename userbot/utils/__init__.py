f6362d9b7672cdc3846ecd6e426f5fa16fb8035c
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-01-02 15:35:31 +0700

    Typo komtol

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,7 +17,7 @@ from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
-from .tools import ) 
+from .tools import (
    humanbytes, 
    time_formatter, 
    human_to_bytes, 
