c10191d385109195f7e635497c502b73fce13d4b
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-01-02 15:28:36 +0700

    Jangan update komtol

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,4 +17,15 @@ from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
-from .tools import humanbytes, time_formatter, human_to_bytes, run_cmd, md5, post_to_telegraph, take_screen_shot, runcmd
+from .tools import ) 
+   humanbytes, 
+   time_formatter, 
+   human_to_bytes, 
+   run_cmd, 
+   md5,
+   edit_delete,
+   edit_or_reply,
+   post_to_telegraph, 
+   take_screen_shot, 
+   runcmd,
+)
