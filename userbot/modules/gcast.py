393818176d76d493e3943ebc8a1e6269833c090f
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-01-29 05:25:05 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -14,9 +14,21 @@
 from userbot import CMD_HELP, DEVS
 from userbot.events import register
 
-GCAST_BLACKLIST = get(
-    "https://raw.githubusercontent.com/Randi356/Reforestation/master/blacklistgcast.json"
-).json()
+GCAST_BLACKLIST = [
+    -1001473548283,  # SharingUserbot
+    -1001433238829,  # TedeSupport
+    -1001476936696,  # AnosSupport
+    -1001327032795,  # UltroidSupport
+    -1001294181499,  # UserBotIndo
+    -1001419516987,  # VeezSupportGroup
+    -1001209432070,  # GeezSupportGroup
+    -1001296934585,  # X-PROJECT BOT
+    -1001481357570,  # UsergeOnTopic
+    -1001459701099,  # CatUserbotSupport
+    -1001109837870,  # TelegramBotIndonesia
+    -1001752592753,  # Skyzusupport
+    -1001273141346,  # Remix Support Vegeta
+]
 
 
 @register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
