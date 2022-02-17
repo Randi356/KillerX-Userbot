cfdb5af1d3b00a3b3cce984be4d8f06b57076a7c
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-02-17 19:11:04 +0700

    ```scripts```

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -15,11 +15,11 @@ TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
 tg_sendinfo "<code>I am gonna merge staging into VEGETA-USERBOT</code>"
 cd
-git clone https://github.com/mkaraniya/OpenUserBot.git
-cd OpenUserBot
-git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com/mkaraniya/OpenUserBot.git
+git clone https://github.com/Randi356/Vegeta-Userbot.git
+cd Vegeta-Userbot
+git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com/Randi356/Vegeta-Userbot.git
 git fetch
 git checkout staging
 git pull origin staging
-git push --force origin staging:VEGETA-USERBOT
+git push --force origin staging:Vegeta-Userbot
 tg_sendinfo "<code>I have merged all commits from staging into RAM-UBOT</code>"
