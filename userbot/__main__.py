e1258fac5452cf4644a417ba454845cc67b971a4
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-02-18 15:04:47 +0700

    soon

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,7 +12,7 @@ from telethon.tl.functions.channels import InviteToChannelRequest
 from userbot import ALIVE_NAME, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES
 from userbot.utils.tools import hadeh_ajg
-from userbot.utils.utils import autobot
+
 
 try:
     for module_name in ALL_MODULES:
@@ -40,7 +40,7 @@ async def vegeta_userbot_on():
 
 bot.loop.run_until_complete(vegeta_userbot_on())
 bot.loop.run_until_complete(hadeh_ajg())
-bot.loop.run_until_complete(autobot())
+
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
