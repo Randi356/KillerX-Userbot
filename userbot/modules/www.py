0ee7ff8cbaed4b2c6bf0abc23020c951dc313f01
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-05 23:07:36 +0700

    Bold

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -448,7 +448,7 @@ async def pingme(pong):
     """ For !ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("`Pinging...`")
+    await pong.edit("**pingers powers**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**╰•★★ ℘ơŋɠ ★★•╯**\n"
