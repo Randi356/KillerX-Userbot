2bde12dc3ba93bc288e1c5fed89a9199c2bc6ebb
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-13 15:56:06 +0700

    Update stats.py

--- userbot/modules/core/stats.py
+++ userbot/modules/core/stats.py
@@ -1,24 +1,20 @@
-"""Count the Number of Dialogs you have in your Telegram Account
-Syntax: .stats"""
-import logging
+# Ported By @rencprx
+
 import time
 
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
-from telethon.tl.types import Channel, Chat, User
+from telethon.tl.types import Channel, User, Chat
 
+from ..help import add_help_item
+from userbot.utils import inline_mention
 from userbot.events import register
 
-logging.basicConfig(
-    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-    level=logging.WARNING)
-logger = logging.getLogger(__name__)
 
-
-@register(outgoing=True, pattern=r"^.stats(?: |$)(.*)")
+@register(outgoing=True, pattern=f'^.stats')
 async def stats(event: NewMessage.Event) -> None:  # pylint: disable = R0912, R0914, R0915
     """Command to get stats about the account"""
-    await event.edit('`Sedang Mencari info pengguna`')
+    waiting_message = await event.edit('Collecting stats. This might take a while.')
     start_time = time.time()
     private_chats = 0
     bots = 0
@@ -30,13 +26,14 @@ async def stats(event: NewMessage.Event) -> None:  # pylint: disable = R0912, R0
     creator_in_channels = 0
     unread_mentions = 0
     unread = 0
+    largest_group_member_count = 0
+    largest_group_with_admin = 0
     dialog: Dialog
     async for dialog in event.client.iter_dialogs():
         entity = dialog.entity
 
         if isinstance(entity, Channel):
-            # participants_count = (await event.get_participants(dialog,
-            # limit=0)).total
+            # participants_count = (await event.get_participants(dialog, limit=0)).total
             if entity.broadcast:
                 broadcast_channels += 1
                 if entity.creator or entity.admin_rights:
@@ -72,39 +69,30 @@ async def stats(event: NewMessage.Event) -> None:  # pylint: disable = R0912, R0
     stop_time = time.time() - start_time
 
     full_name = inline_mention(await event.client.get_me())
-    response = f'🔸 **Stats for {full_name}** \n\n'
-    response += f'**Private Chats:** {private_chats} \n'
-    response += f'   • `Users: {private_chats - bots}` \n'
-    response += f'   • `Bots: {bots}` \n'
-    response += f'**Groups:** {groups} \n'
-    response += f'**Channels:** {broadcast_channels} \n'
-    response += f'**Admin in Groups:** {admin_in_groups} \n'
-    response += f'   • `Creator: {creator_in_groups}` \n'
-    response += f'   • `Admin Rights: {admin_in_groups - creator_in_groups}` \n'
-    response += f'**Admin in Channels:** {admin_in_broadcast_channels} \n'
-    response += f'   • `Creator: {creator_in_channels}` \n'
-    response += f'   • `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n'
-    response += f'**Unread:** {unread} \n'
-    response += f'**Unread Mentions:** {unread_mentions} \n\n'
-    response += f'__It Took:__ {stop_time:.02f}s \n'
+    response = f'**Stats for {full_name}** \n'
+    response += f'    **Private Chats:** {private_chats} \n'
+    response += f'        **Users:** {private_chats - bots} \n'
+    response += f'        **Bots:** {bots} \n'
+    response += f'    **Groups:** {groups} \n'
+    response += f'    **Channels:** {broadcast_channels} \n'
+    response += f'    **Admin in Groups:** {admin_in_groups} \n'
+    response += f'        **Creator:** {creator_in_groups} \n'
+    response += f'        **Admin Rights:** {admin_in_groups - creator_in_groups} \n'
+    response += f'    **Admin in Channels:** {admin_in_broadcast_channels} \n'
+    response += f'        **Creator:** {creator_in_channels} \n'
+    response += f'        **Admin Rights:** {admin_in_broadcast_channels - creator_in_channels} \n'
+    response += f'    **Unread:** {unread} \n'
+    response += f'    **Unread Mentions:** {unread_mentions} \n\n'
+    response += f'__Took:__ {stop_time:.02f}s \n'
 
     await event.edit(response)
 
 
-def make_mention(user):
-    if user.username:
-        return f"@{user.username}"
-    else:
-        return inline_mention(user)
-
-
-def inline_mention(user):
-    full_name = user_full_name(user) or "No Name"
-    return f"[{full_name}](tg://user?id={user.id})"
-
-
-def user_full_name(user):
-    names = [user.first_name, user.last_name]
-    names = [i for i in list(names) if i]
-    full_name = ' '.join(names)
-    return full_name
+add_help_item(
+    "stats",
+    "Core",
+    "Get some basic Telegram stats about yourself.",
+    """
+    `.stats`
+    """
+)
