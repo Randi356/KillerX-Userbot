fd93489f80e4f01c7d5731eea74179d1e49c7908
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-13 16:38:31 +0700

    Update sangmata.py

--- userbot/modules/misc/sangmata.py
+++ userbot/modules/misc/sangmata.py
@@ -1,65 +1,53 @@
-# Port to userbot by @MoveAngel
-# Ported By @rencprx 
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Port to @renpcrx by @MoveAngel
 
+from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+
+from ..help import add_help_item
+from userbot import bot
 from userbot.events import register
-from asyncio.exceptions import TimeoutError
 
 
-@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
+@register(outgoing=True, pattern="^.sg(?: |$)(.*)")
 async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Reply Ke Pesan Pengguna Dulu```")
-        return
-    message = await steal.get_reply_message()
+       await steal.edit("`Reply to any user message.`")
+       return
+    reply_message = await steal.get_reply_message()
+    if not reply_message.text:
+       await steal.edit("`reply to text message`")
+       return
     chat = "@SangMataInfo_bot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
-    if message.sender.bot:
-        await steal.edit("```Reply Ke Pesan Pengguna dulu```")
-        return
-    await steal.edit("WHACKING EYES PROO....!!.")
-    try:
-        async with bot.conversation(chat) as conv:
-            try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await steal.reply(
-                    "```Mohon Unblock @sangmatainfo_bot Dan Coba Scan Kembali.```"
-                )
-                return
-            if r.text.startswith("Name"):
-                respond = await conv.get_response()
-                await steal.edit(f"`{r.message}`")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
-                )
-                return
-            if response.text.startswith("No records") or r.text.startswith(
-                "No records"
-            ):
-                await steal.edit("```Masa Gua Gak Nemu Riwayat Namanya si ngentot, Wah si anjing belom pernah ganti nama kayak nya su!!```")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id]
-                )
-                return
-            else:
-                respond = await conv.get_response()
-                await steal.edit(f"```{response.message}```")
-            await steal.client.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await steal.edit("`404 error`")
+    if reply_message.sender.bot:
+       await steal.edit("`Reply to actual users message.`")
+       return
+    await steal.edit("`Sit tight while I steal some data from NASA`")
+    async with bot.conversation(chat) as conv:
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await bot.forward_messages(chat, reply_message)
+              response = await response
+          except YouBlockedUserError:
+              await steal.reply("`Please unblock @sangmatainfo_bot and try again`")
+              return
+          if response.text.startswith("Forward"):
+             await steal.edit("`can you kindly disable your forward privacy settings for good?`")
+          else:
+             await steal.edit(f"{response.message.message}")
 
 
-CMD_HELP.update({
-    "sangmata":
-        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sg`\
-          \n↳ : Mendapatkan Riwayat Nama Pengguna Yang Di Scan."
-})
+add_help_item(
+    "sangmata",
+    "Misc",
+    "See your or friend names history",
+    """
+    `.sg`
+    """
+)
