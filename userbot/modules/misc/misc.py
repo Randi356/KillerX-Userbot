0ac8922ab53c4a2ffde11fdd1e865cae2adb68c2
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-13 16:52:31 +0700

    ```fixed small```

--- userbot/modules/misc/misc.py
+++ userbot/modules/misc/misc.py
@@ -1,33 +1,48 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You can find misc modules, which dont fit in anything xD
+""" Userbot module for other small commands. """
+
 from random import randint
-from time import sleep
+from asyncio import sleep
 from os import execl
-import asyncio
 import sys
 import os
 import io
 import sys
-from userbot import ALIVE_NAME, UPSTREAM_REPO_URL, BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+import json
+from ..help import add_help_item
+from userbot import BOTLOG, BOTLOG_CHATID, bot, UPSTREAM_REPO_URL
 from userbot.events import register
-from userbot.utils import time_formatter
-import urllib
-import requests
-from bs4 import BeautifulSoup
-import re
-from PIL import Image
 
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-REPOLINK = str(
-    UPSTREAM_REPO_URL) if UPSTREAM_REPO_URL else "https://github.com/Randi356/Vegeta-Userbot"
-# ============================================
-
-opener = urllib.request.build_opener()
-useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
-opener.addheaders = [('User-agent', useragent)]
+@register(outgoing=True, pattern="^.community$")
+async def bot_community(community):
+    """ For .community command, just returns OG Paperplane's group link. """
+    await community.edit(
+        "Join RaphielGang's awesome userbot community: @VegetaSupports"
+        "\nDo note that TG-UBotX is an unoficial fork of their "
+        "Paperplane project and it may get limited or no support for bugs.")
 
 
-@register(outgoing=True, pattern="^.random")
+@register(outgoing=True, pattern="^.creator$")
+async def creator(e):
+    await e.edit("Here's my God: [Rendy](https://t.me/rencprx)")
+
+
+@register(outgoing=True, pattern="^.readme$")
+async def reedme(e):
+    await e.edit(
+        "**Here's something for you to read:**\n"
+        "\n[Vegeta - Wiki](https:// github.com/Randi356/Vegeta-Userbot/tree/dev)"
+        "\n[Setup Guide - GDive](https://telegra.ph/How-To-Setup-GDrive-11-02)"
+        "\n[News Channel](https://t.me/TeamVupdates)")
+
+
+@register(outgoing=True, pattern="^\.random")
 async def randomise(items):
     """ For .random command, get a random item from the list of items. """
     itemo = (items.text[8:]).split()
@@ -41,58 +56,50 @@ async def randomise(items):
                      itemo[index] + "`")
 
 
-@register(outgoing=True, pattern="^.sleep ([0-9]+)$")
+@register(outgoing=True, pattern="^\.sleep( [0-9]+)?$")
 async def sleepybot(time):
     """ For .sleep command, let the userbot snooze for a few second. """
-    counter = int(time.pattern_match.group(1))
-    await time.edit("`I am sulking and snoozing...`")
-    if BOTLOG:
-        str_counter = time_formatter(counter)
-        await time.client.send_message(
-            BOTLOG_CHATID,
-            f"You put the bot to sleep for {str_counter}.",
-        )
-    sleep(counter)
-    await time.edit("`OK, I'm awake now.`")
+    message = time.text
+    if " " not in time.pattern_match.group(1):
+        await time.reply("Syntax: `.sleep [seconds]`")
+    else:
+        counter = int(time.pattern_match.group(1))
+        await time.edit("`I am sulking and snoozing....`")
+        await sleep(2)
+        if BOTLOG:
+            await time.client.send_message(
+                BOTLOG_CHATID,
+                "You put the bot to sleep for " + str(counter) + " seconds",
+            )
+        await sleep(counter)
+        await time.edit("`OK, I'm awake now.`")
 
 
-@register(outgoing=True, pattern="^.shutdown$")
+@register(outgoing=True, pattern="^\.shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Mematikan Vegeta-Userbot....`")
-    await asyncio.sleep(7)
-    await event.delete()
+    await event.edit("`Goodbye *Windows XP shutdown sound*....`")
     if BOTLOG:
         await event.client.send_message(BOTLOG_CHATID, "#SHUTDOWN \n"
-                                        "`Userbot Telah Dimatikan`")
+                                        "Bot shut down")
     await bot.disconnect()
 
 
-@register(outgoing=True, pattern="^.restart$")
+@register(outgoing=True, pattern="^\.restart$")
 async def killdabot(event):
-    await event.edit("`Restarting Vegeta-Userbot...`")
-    await asyncio.sleep(10)
-    await event.delete()
+    await event.edit("`Hold tight! I just need a second to be back up....`")
     if BOTLOG:
-        await event.client.send_message(BOTLOG_CHATID, "#RESTARTBOT \n"
-                                        "`Userbot Telah Di Restart`")
-    await bot.disconnect()
+        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n"
+                                                       "Bot Restarted")
+    await event.client.disconnect()
     # Spin a new instance of bot
     execl(sys.executable, sys.executable, *sys.argv)
     # Shut the existing one down
     exit()
 
 
-@register(outgoing=True, pattern="^.readme$")
-async def reedme(e):
-    await e.edit(
-        "Here's Something for You to Read :\n"
-        "\n[❄️ Vegeta-Userbot Repo](https://github.com/Randi356/Vegeta-Userbot/blob/Vegeta-Userbot/README.md)"
-        "\n[❄️Setup Guide - Basic](https://telegra.ph/How-to-host-a-Telegram-Userbot-11-02)"
-        "\n[❄️Special - Note](https://telegra.ph/Special-Note-11-02)")
-
-
-@register(outgoing=True, pattern="^.repeat (.*)")
+# Copyright (c) Gegham Zakaryan | 2019
+@register(outgoing=True, pattern="^\.repeat (.*)")
 async def repeat(rep):
     cnt, txt = rep.pattern_match.group(1).split(' ', 1)
     replyCount = int(cnt)
@@ -106,30 +113,14 @@ async def repeat(rep):
     await rep.edit(replyText)
 
 
-@register(outgoing=True, pattern="^.repo$")
+@register(outgoing=True, pattern="^\.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "**Hey**, I am using **🌸Vegeta-Userbot🌸** \n"
-        "❄️━━━━━━━━━━━━━━━━━━━━━━卍\n"
-        "⇼ **Repo Userbot :** [Repo](https://github.com/Randi356/Vegeta-Userbot)\n"
-        "⇼ **Owner Bot       :** [Rendy](t.me/CuteInspire)\n"
-        "❄️━━━━━━━━━━━━━━━━━━━━━━卍\n"
-        "⇼ **Instagram       :** [instagram](instagram.com/yotteno)\n"
-        "⇼ **Support           :** [Channel](https://t.me/RendyProjects)\n"
-        "❄️━━━━━━━━━━━━━━━━━━━━━━卍\n"
-    )
+        f"Click [here](https://github.com/Vegeta-Userbot/tree/dev) to open my userbot's repository.")
 
 
-@register(outgoing=True, pattern=r"^\.string$")
-async def repo_is_here(wannasee):
-    """For .repo command, just returns the repo URL."""
-    await wannasee.edit(
-        f"⇼ **GET STRING SESSION TELEGRAM :** [KLIK DISINI](https://replit.com/@Randi356/Vegeta-String)\n"
-    )
-
-
-@register(outgoing=True, pattern="^.raw$")
+@register(outgoing=True, pattern="^\.raw$")
 async def raw(event):
     the_real_message = None
     reply_to_id = None
@@ -153,142 +144,40 @@ async def raw(event):
             caption="`Here's the decoded message data !!`")
 
 
-@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
-async def okgoogle(img):
-    """ For .reverse command, Google search images and stickers. """
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
+add_help_item(
+    "misc",
+    "Misc",
+    "Some misc commands.",
+    """
+    `.raw`
+    **Usage:** Get detailed JSON-like formatted data about replied message.
 
-    message = await img.get_reply_message()
-    if message and message.media:
-        photo = io.BytesIO()
-        await bot.download_media(message, photo)
-    else:
-        await img.edit("`Harap Reply Di Gambar...`")
-        return
+    `.random` <item1> <item2> ... <itemN>
+    **Usage:** Get a random item from the list of items.
 
-    if photo:
-        await img.edit("`Processing...`")
-        try:
-            image = Image.open(photo)
-        except OSError:
-            await img.edit('`Gambar tidak di dukung`')
-            return
-        name = "okgoogle.png"
-        image.save(name, "PNG")
-        image.close()
-        # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
-        searchUrl = 'https://www.google.com/searchbyimage/upload'
-        multipart = {
-            'encoded_image': (name, open(name, 'rb')),
-            'image_content': ''
-        }
-        response = requests.post(searchUrl,
-                                 files=multipart,
-                                 allow_redirects=False)
-        fetchUrl = response.headers['Location']
+    `.repeat` <no.> <text>
+    **Usage:** Repeats the text for a number of times. Don't confuse this with spam tho.
 
-        if response != 400:
-            await img.edit("`Image successfully uploaded to Google. Maybe.`"
-                           "\n`Parsing source now. Maybe.`")
-        else:
-            await img.edit("`Google told me to fuck off.`")
-            return
+    `.restart`
+    **Usage:** Restarts the bot!
 
-        os.remove(name)
-        match = await ParseSauce(fetchUrl +
-                                 "&preferences?hl=en&fg=1#languages")
-        guess = match['best_guess']
-        imgspage = match['similar_images']
+    `.readme`
+    **Usage:** Provide links to setup the userbot and it's modules.
 
-        if guess and imgspage:
-            await img.edit(f"[{guess}]({fetchUrl})\n\n`Looking for images...`")
-        else:
-            await img.edit("`Couldn't find anything for your uglyass.`")
-            return
+    `.creator`
+    **Usage:** Know who created this awesome userbot!
 
-        if img.pattern_match.group(1):
-            lim = img.pattern_match.group(1)
-        else:
-            lim = 3
-        images = await scam(match, lim)
-        yeet = []
-        for i in images:
-            k = requests.get(i)
-            yeet.append(k.content)
-        try:
-            await img.client.send_file(entity=await
-                                       img.client.get_input_entity(img.chat_id
-                                                                   ),
-                                       file=yeet,
-                                       reply_to=img)
-        except TypeError:
-            pass
-        await img.edit(
-            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})")
+    `.community`
+    **Usage:** Join the awesome Paperplane userbot community!
 
+    `.repo`
+    **Usage:** If you are curious what makes the userbot work, this is what you need.
 
-async def ParseSauce(googleurl):
-    """Parse/Scrape the HTML code for the info we want."""
+    `.shutdown`
+    **Usage:** Sometimes you need to shut down your bot. Sometimes you just hope to
+    hear Windows XP shutdown sound... but you don't.
 
-    source = opener.open(googleurl).read()
-    soup = BeautifulSoup(source, 'html.parser')
-
-    results = {'similar_images': '', 'best_guess': ''}
-
-    try:
-        for similar_image in soup.findAll('input', {'class': 'gLFyf'}):
-            url = 'https://www.google.com/search?tbm=isch&q=' + \
-                urllib.parse.quote_plus(similar_image.get('value'))
-            results['similar_images'] = url
-    except BaseException:
-        pass
-
-    for best_guess in soup.findAll('div', attrs={'class': 'r5a77d'}):
-        results['best_guess'] = best_guess.get_text()
-
-    return results
-
-
-async def scam(results, lim):
-
-    single = opener.open(results['similar_images']).read()
-    decoded = single.decode('utf-8')
-
-    imglinks = []
-    counter = 0
-
-    pattern = r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
-    oboi = re.findall(pattern, decoded, re.I | re.M)
-
-    for imglink in oboi:
-        counter += 1
-        if not counter >= int(lim):
-            imglinks.append(imglink)
-        else:
-            break
-
-    return imglinks
-
-
-CMD_HELP.update({
-    "random": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.random <item1> <item2> ... <itemN>`\
-    \n↳ : Dapatkan item acak dari daftar item.",
-    "sleep": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sleep <seconds>`\
-    \n↳ : `.sleep`\
-    \n  •  **Function : Biarkan Vegeta-Userbot tidur selama beberapa detik.",
-    "shutdown": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.shutdown`\
-    \n↳ : Mematikan bot",
-    "repo": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.repo`\
-    \n↳ : Menampilan link Repository Vegeta-Userbot.",
-    "string": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.string`\
-    \n↳: Menampilkan link String Vegeta-Userbot",
-    "readme": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙 `.readme`\
-    \n↳ : Menyediakan tautan untuk mengatur userbot dan modulnya.",
-    "repeat": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.repeat <no> <text>`\
-    \n↳ : Mengulangi teks untuk beberapa kali. Jangan bingung ini dengan spam tho.",
-    "restart": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.restart`\
-    \n↳ : Merestart bot",
-    "raw": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.raw`\
-    \n↳ : Dapatkan data berformat seperti JSON terperinci tentang pesan yang dibalas.",
-})
+    `.sleep` <seconds>
+    **Usage:** Userbots get tired too. Let yours snooze for a few seconds.
+    """
+)
