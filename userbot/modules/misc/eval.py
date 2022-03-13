07e1ef1a38d385c9ef18da12afaf02d9e2e48389
Cute Inspire <63757267+Randi356@users.noreply.github.com>
2022-03-13 16:46:03 +0700

    Update eval.py

--- userbot/modules/misc/eval.py
+++ userbot/modules/misc/eval.py
@@ -1,193 +1,152 @@
-# Ported By @rencprx
+# Ported By @rencprx 
 
-import asyncio
+import sys
+import ast
+import contextlib
 from os import remove
-from sys import executable
+from io import StringIO
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TERM_ALIAS
+from ..help import add_help_item
+from userbot import BOTLOG, BOTLOG_CHATID, bot
 from userbot.events import register
+from userbot.utils.tgdoc import *
 
 
-@register(outgoing=True, pattern=r"^\.eval(?: |$)(.*)")
-async def evaluate(query):
-    if query.is_channel and not query.is_group:
-        return await query.edit("`Eval isn't permitted on channels`")
+@register(outgoing=True, pattern=r"^\.eval(?:\s+([\S\s]+)|$)")
+async def evaluate(e):
+    """ For .eval command, evaluates the given Python expression. """
+    reply = await e.get_reply_message()
+    if e.is_channel and not e.is_group:
+        await e.edit("`Eval isn't permitted on channels`")
+        return
 
-    if query.pattern_match.group(1):
-        expression = query.pattern_match.group(1)
+    if e.pattern_match.group(1):
+        expression = e.pattern_match.group(1)
+    elif reply:
+        expression = reply.message
     else:
-        return await query.edit("``` Give an expression to evaluate. ```")
+        await e.edit(str(Bold("Give an expression to evaluate")))
+        return
 
-    if expression in ("userbot.session", "config.env"):
-        return await query.edit("`That's a dangerous operation! Not Permitted!`")
+    head = Section(Bold("Query:"), Code(expression), indent=0)
 
     try:
-        evaluation = str(eval(expression))
+        response, out = await async_eval(expression, bot=bot, event=e, reply=reply)
+        evaluation = str(response)
+
         if evaluation:
             if isinstance(evaluation, str):
                 if len(evaluation) >= 4096:
                     file = open("output.txt", "w+")
                     file.write(evaluation)
                     file.close()
-                    await query.client.send_file(
-                        query.chat_id,
+                    await e.client.send_file(
+                        e.chat_id,
                         "output.txt",
-                        reply_to=query.id,
+                        reply_to=e.id,
                         caption="`Output too large, sending as file`",
                     )
                     remove("output.txt")
                     return
-                await query.edit(
-                    "**Query: **\n`"
-                    f"{expression}"
-                    "`\n**Result: **\n`"
-                    f"{evaluation}"
-                    "`"
-                )
-        else:
-            await query.edit(
-                "**Query: **\n`"
-                f"{expression}"
-                "`\n**Result: **\n`No Result Returned/False`"
-            )
+
+        result = Section(Bold("Result:"), Code(response), indent=0)
+        output = Section(Bold("Output:"), Code(out), indent=0)
+        section = Section(
+            head,
+            result if response else None,
+            output if out else None,
+            indent=0,
+            spacing=2
+        )
+
+        await e.edit(str(section))
+
     except Exception as err:
-        await query.edit(
-            "**Query: **\n`" f"{expression}" "`\n**Exception: **\n" f"`{err}`"
-        )
+        error = SubSection(Bold("Error:"), Code(str(err)), indent=0)
+        section = Section(head, error, indent=0, spacing=2)
+        await e.edit(str(section))
 
     if BOTLOG:
-        await query.client.send_message(
-            BOTLOG_CHATID, f"Eval query {expression} was executed successfully"
-        )
-
-
-@register(outgoing=True, pattern=r"^\.exec(?: |$)([\s\S]*)")
-async def run(run_q):
-    code = run_q.pattern_match.group(1)
-
-    if run_q.is_channel and not run_q.is_group:
-        return await run_q.edit("`Exec isn't permitted on channels!`")
-
-    if not code:
-        return await run_q.edit(
-            "``` At least a variable is required to"
-            "execute. Use .help exec for an example.```"
-        )
-
-    if code in ("userbot.session", "config.env"):
-        return await run_q.edit("`That's a dangerous operation! Not Permitted!`")
-
-    if len(code.splitlines()) <= 5:
-        codepre = code
-    else:
-        clines = code.splitlines()
-        codepre = (
-            clines[0] +
-            "\n" +
-            clines[1] +
-            "\n" +
-            clines[2] +
-            "\n" +
-            clines[3] +
-            "...")
-
-    command = "".join(f"\n {l}" for l in code.split("\n.strip()"))
-    process = await asyncio.create_subprocess_exec(
-        executable,
-        "-c",
-        command.strip(),
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-    if result:
-        if len(result) > 4096:
-            file = open("output.txt", "w+")
-            file.write(result)
-            file.close()
-            await run_q.client.send_file(
-                run_q.chat_id,
-                "output.txt",
-                reply_to=run_q.id,
-                caption="`Output too large, sending as file`",
-            )
-            remove("output.txt")
-            return
-        await run_q.edit(
-            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`" f"{result}" "`"
-        )
-    else:
-        await run_q.edit(
-            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`No Result Returned/False`"
-        )
-
-    if BOTLOG:
-        await run_q.client.send_message(
-            BOTLOG_CHATID, "Exec query " + codepre + " was executed successfully"
-        )
-
-
-@register(outgoing=True, pattern=r"^\.term(?: |$)(.*)")
-async def terminal_runner(term):
-    curruser = TERM_ALIAS
-    command = term.pattern_match.group(1)
-    try:
-        from os import geteuid
-
-        uid = geteuid()
-    except ImportError:
-        uid = "This ain't it chief!"
-
-    if term.is_channel and not term.is_group:
-        return await term.edit("`Term commands aren't permitted on channels!`")
-
-    if not command:
-        return await term.edit(
-            "``` Give a command or use .help term for an example.```"
-        )
-
-    if command in ("userbot.session", "config.env"):
-        return await term.edit("`That's a dangerous operation! Not Permitted!`")
-
-    process = await asyncio.create_subprocess_shell(
-        command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-    if len(result) > 4096:
-        output = open("output.txt", "w+")
-        output.write(result)
-        output.close()
-        await term.client.send_file(
-            term.chat_id,
-            "output.txt",
-            reply_to=term.id,
-            caption="`Output too large, sending as file`",
-        )
-        remove("output.txt")
-        return
-
-    if uid == 0:
-        await term.edit("`" f"{curruser}:~# {command}" f"\n{result}" "`")
-    else:
-        await term.edit("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
-
-
-"""
-    if BOTLOG:
-        await term.client.send_message(
+        await e.client.send_message(
             BOTLOG_CHATID,
-            "Terminal Command " + command + " was executed sucessfully",
-        )
-"""
+            f"#EVAL\nEval query ```{expression}``` was executed successfully")
 
-CMD_HELP.update({"eval": ">`.eval 2 + 3`"
-                 "\nUsage: Evalute mini-expressions.",
-                 "exec": ">`.exec print('hello')`"
-                 "\nUsage: Execute small python scripts.",
-                 "term": ">`.term <cmd>`"
-                 "\nUsage: Run bash commands and scripts on your server.",
-                 })
+
+# https://stackoverflow.com/a/57349931/974936
+async def async_eval(code, **kwargs):
+    # Note to self: please don't set globals here as they will be lost.
+    # Don't clutter locals
+    locs = {}
+    # Restore globals later
+    globs = globals().copy()
+    # This code saves __name__ and __package into a kwarg passed to the function.
+    # It is set before the users code runs to make sure relative imports work
+    global_args = "_globs"
+    while global_args in globs.keys():
+        # Make sure there's no name collision, just keep prepending _s
+        global_args = "_" + global_args
+    kwargs[global_args] = {}
+    for glob in ["__name__", "__package__"]:
+        # Copy data to args we are sending
+        kwargs[global_args][glob] = globs[glob]
+
+    root = ast.parse(code, 'exec')
+    code = root.body
+    if isinstance(code[-1], ast.Expr):  # If we can use it as a lambda return (but multiline)
+        code[-1] = ast.copy_location(ast.Return(code[-1].value), code[-1])  # Change it to a return statement
+    # globals().update(**<global_args>)
+    glob_copy = ast.Expr(ast.Call(func=ast.Attribute(value=ast.Call(func=ast.Name(id='globals', ctx=ast.Load()),
+                                                                    args=[], keywords=[]),
+                                                     attr='update', ctx=ast.Load()),
+                                  args=[], keywords=[ast.keyword(arg=None,
+                                                                 value=ast.Name(id=global_args, ctx=ast.Load()))]))
+    glob_copy.lineno = 0
+    glob_copy.col_offset = 0
+    ast.fix_missing_locations(glob_copy)
+    code.insert(0, glob_copy)
+    args = []
+    for a in list(map(lambda x: ast.arg(x, None), kwargs.keys())):
+        a.lineno = 0
+        a.col_offset = 0
+        args += [a]
+    fun = ast.AsyncFunctionDef('tmp', ast.arguments(args=[], vararg=None, kwonlyargs=args, kwarg=None, defaults=[],
+                                                    kw_defaults=[None for i in range(len(args))]), code, [], None)
+    fun.lineno = 0
+    fun.col_offset = 0
+    mod = ast.Module([fun])
+    comp = compile(mod, '<string>', 'exec')
+
+    exec(comp, {}, locs)
+
+    with temp_stdio() as out:
+        result = await locs["tmp"](**kwargs)
+        try:
+            globals().clear()
+            # Inconsistent state
+        finally:
+            globals().update(**globs)
+        return result, out.getvalue()
+
+
+# Create a temporary stdio
+@contextlib.contextmanager
+def temp_stdio(stdout=None, stderr=None):
+    old_out = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old_out
+
+
+add_help_item(
+    "eval",
+    "Misc",
+    "Evaluates a small Python expression using `eval()`.",
+    """
+    `.eval (expression)`
+    
+    Or, in response to a message
+    `.eval`
+    """
+)
